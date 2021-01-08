2b433ebf7bfcd69e6b4282e732309d2e4954133c
Luis Fernando <luciusfernatore@outlook.com>
2021-01-08 12:32:44 -0500

    "A little bit of changes 1.0"

--- mentores_mvp1.js
+++ mentores_mvp1.js
@@ -1,16 +1,31 @@
 function myFunction() {
-    var input, filter, container, contact, a, i, txtValue;
+    let input;
+    let filter;
+    let Ulcontainer;
+    let listElements;
+    let a;
+    let i;
+    let txtValue;
+    let contactContainer;
+
+    //get tags
+    //Search bar input
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    container = document.getElementById("myUL");
-    contact = container.getElementsByClass("contact");
-    for (i = 0; i < contact.length; i++) {
-        a = contact[i].getElementsByTagName("a")[0];
+
+    //Grab elements from the html
+    Ulcontainer = document.getElementsByClass("row_skills");
+    listElements = Ulcontainer.getElementsByTagName("li");
+
+    //traverse collection TAGS
+    for (i = 0; i < listElements.length; ++i) {
+        a = listElements[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            contact[i].style.display = "";
+        if (txtValue.indexOf(filter) > -1) {
+            listElements[i].toUpperCase.style.display = "";
         } else {
-            contact[i].style.display = "none";
+            listElements[i].style.display = "none";
         }
     }
+
 }
