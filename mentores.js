84877e211f2148d14bc3eedd4ce282e83c5c5cc5
Miuieki <52682078+Miuieki@users.noreply.github.com>
2021-01-13 17:53:05 -0500

    Miyu branch (#12)
    * Change UI
    
    * Conctacts added
    
    * Change design after searchbar works
    
    * Agregando las imagenes en el buscador de Edward
    
    * Add photos mentores Edward
    
    * changes index name
    
    * initial commit
    
    * Luis Images uploaded
    
    * Delete mentores_mvp1.html
    
    Co-authored-by: Luis Fernando <luciusfernatore@outlook.com>
    Co-authored-by: TheLuisADL <luis.bolivar@avaldigitallabs.com>

--- mentores.js
+++ mentores.js
@@ -41,6 +41,8 @@ const validateValue = (value) => {
     return valueToClear.replace(/ /gi, "");
 }
 
+
 const removeAccents = (str) => {
     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 } 
+
