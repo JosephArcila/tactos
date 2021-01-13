6e19d3f16b90076486734d31890111fef458bfef
Joseph Arcila <39535338+JosephArcila@users.noreply.github.com>
2021-01-13 12:26:04 -0500

    Agregar 80 contactos agregar index.html (#9)

---
+++ mentores.js
@@ -0,0 +1,25 @@
+function Search_bar() {
+    let input;
+    let Ulcontainer;
+    let UIContactContainer
+
+    //get tags
+    //Search bar input
+    input = document.getElementById("myInput").value;
+    input = input.toLowerCase();
+
+    //Grab elements from the html
+    Ulcontainer = document.getElementsByClassName("row_skills");
+    UIContactContainer = document.getElementsByClassName("contact");
+
+    //traverse collection TAGS
+    for (let i = 0; i < Ulcontainer.length; ++i) {
+        if (Ulcontainer[i].innerHTML.toLowerCase().includes(input)) {
+            //show the division
+            UIContactContainer[i].style.display = "list-item";
+        } else {
+            //Hide the division
+            UIContactContainer[i].style.display = "none";
+        }
+    }
+}
